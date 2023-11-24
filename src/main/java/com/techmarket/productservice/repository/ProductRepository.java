dd49bbf6c9542c3b0e9d79473919e51cce2d1232
juliparodi <julianparodi19@gmail.com>
2023-11-24 20:30:03 -0300

    initial implementation for product service

---
+++ src/main/java/com/techmarket/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.techmarket.productservice.repository;
+
+import com.techmarket.productservice.model.entities.Product;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository  extends MongoRepository<Product, ObjectId>  {
+}
